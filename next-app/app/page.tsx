243e8605c71d864561a46ed3ede376f7b04b9e94
damienabrahams0569 <damien.abrahams@diversus.com.au>
2023-09-22 13:24:09 +0800

    Add Lambda

--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -1,18 +1,21 @@
-'use client';
+'use client'; // Needed for withAuthenticator function
 
-import { WithAuthenticatorProps, withAuthenticator } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
-import Link from 'next/link';
+import {
+  WithAuthenticatorProps,
+  withAuthenticator,
+} from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react/styles.css'
+import Link from 'next/link'
 
 function getRenderDate() {
-  const renderedAt = new Date();
+  const renderedAt = new Date()
   const formattedBuildDate = renderedAt.toLocaleDateString('en-US', {
     dateStyle: 'long',
-  });
+  })
   const formattedBuildTime = renderedAt.toLocaleTimeString('en-US', {
     timeStyle: 'long',
-  });
-  return `${formattedBuildDate} at ${formattedBuildTime}`;
+  })
+  return `${formattedBuildDate} at ${formattedBuildTime}`
 }
 
 function Home({ signOut, user }: WithAuthenticatorProps) {
@@ -21,12 +24,20 @@ function Home({ signOut, user }: WithAuthenticatorProps) {
   return (
     <main>
       <h1>Girl Talk {renderedAt}</h1>
-      <h2>Logged in as {user?.username ?? '_'}</h2>
+      <p className='py-2 text-green-400'>
+        Logged in as {user?.username ?? '_'}
+      </p>
       <p className='py-2 text-blue-400'>Page rendered {renderedAt}</p>
-      <Link href='/users'>Users</Link>
-      <button className='bg-slate-600' onClick={signOut}>Logout</button>
+      <Link className='underline' href='/users'>
+        Users
+      </Link>
+      <div>
+        <button className='bg-slate-600 mt-2' onClick={signOut}>
+          Logout
+        </button>
+      </div>
     </main>
-  );
+  )
 }
 
 export default withAuthenticator(Home)
